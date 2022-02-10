4125f05cb1de49a07c54a983282d4057dc7aec73
BMWire <bmwemawire@gmail.com>
2022-02-10 12:58:21 +0300

    feat: render the blog markdown content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -13,6 +13,8 @@ export default function Post({ postData })
             {postData.id}
             <br />
             {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     )
 }
@@ -29,7 +31,7 @@ export async function getStaticPaths()
 
 export async function getStaticProps({ params })
 {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
